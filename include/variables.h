d10272cb84e7ba8f70c8003dc40ee44dcf06528e
Cameron Hall <cameronghall@cox.net>
2018-03-01 15:43:35 -0600

    move string data to C file

--- include/variables.h
+++ include/variables.h
@@ -1205,9 +1205,9 @@ extern const u16 gUnknown_080D7504[];
 // extern ??? gUnknown_080E834E
 // extern ??? gUnknown_080E836C
 // extern ??? gUnknown_080E838E
-// extern ??? gUnknown_0815A72C
-// extern ??? gUnknown_0815D488
-extern const char *const gUnknown_0815D48C[];
+// extern ??? gMsgHuffmanTable
+// extern ??? gMsgHuffmanTableRoot
+extern const u8 *const gMsgStringTable[];
 // extern ??? gUnknown_08205714
 // extern ??? gUnknown_0820579C
 // extern ??? gUnknown_08205824
