a03e903025668c92aeafe198818f3f652fe420f7
camthesaxman <cameronghall@cox.net>
2018-03-03 13:52:59 -0600

    decompile ctc.s

--- include/types.h
+++ include/types.h
@@ -126,4 +126,14 @@ struct ROMCharStruct
     u16 unk0;
 };
 
+struct UnknownStructCTC
+{
+    struct UnknownStructCTC *unk0;
+    s16 unk4;
+    s16 unk6;
+    u16 unk8;
+    u8 fillerA[2];
+    const void *unkC;
+};
+
 #endif  // GUARD_TYPES_H
