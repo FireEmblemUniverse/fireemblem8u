379f1fff16a3704327a0228f9df6dc39576061d4
Stan H <deindall@gmail.com>
2018-05-20 17:50:33 +0200

    (split) bmusort from code

--- asm/bmcontainer.s
+++ asm/bmcontainer.s
@@ -202,4 +202,60 @@ _0803165A:
 	pop {r1}
 	bx r1
 
+	THUMB_FUNC_START sub_8031660
+sub_8031660: @ 0x08031660
+	push {lr}
+	ldr r0, _0803167C  @ gUnknown_0202BCF0
+	ldrb r0, [r0, #0xe]
+	lsls r0, r0, #0x18
+	asrs r0, r0, #0x18
+	bl GetROMChapterStruct
+	adds r0, #0x82
+	ldrb r0, [r0]
+	cmp r0, #0xff
+	beq _08031680
+	movs r0, #1
+	b _08031682
+	.align 2, 0
+_0803167C: .4byte gUnknown_0202BCF0
+_08031680:
+	movs r0, #0
+_08031682:
+	pop {r1}
+	bx r1
+
+	THUMB_FUNC_START sub_8031688
+sub_8031688: @ 0x08031688
+	push {r4, lr}
+	movs r4, #1
+_0803168C:
+	adds r0, r4, #0
+	bl GetUnitStruct
+	adds r2, r0, #0
+	cmp r2, #0
+	beq _080316B4
+	ldr r1, [r2]
+	cmp r1, #0
+	beq _080316B4
+	ldr r0, [r2, #4]
+	ldr r1, [r1, #0x28]
+	ldr r0, [r0, #0x28]
+	orrs r1, r0
+	movs r0, #0x80
+	lsls r0, r0, #2
+	ands r1, r0
+	cmp r1, #0
+	beq _080316B4
+	adds r0, r2, #0
+	b _080316BC
+_080316B4:
+	adds r4, #1
+	cmp r4, #0x3f
+	ble _0803168C
+	movs r0, #0
+_080316BC:
+	pop {r4}
+	pop {r1}
+	bx r1
+
 .align 2, 0
