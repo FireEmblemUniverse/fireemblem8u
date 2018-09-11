062d302329fa8d9ab4c6685713bc0889af271cf8
Stan H <deindall@gmail.com>
2018-09-11 17:23:10 +0200

    (mu) tab to spaces

--- include/mu.h
+++ include/mu.h
@@ -7,128 +7,128 @@
 */
 
 enum {
-	// MU Magic Constants
+    // MU Magic Constants
 
-	// Maximum simultaneous MU count
-	MU_MAX_COUNT = 4,
+    // Maximum simultaneous MU count
+    MU_MAX_COUNT = 4,
 
-	// Maximum move command count
-	MU_COMMAND_MAX_COUNT = 0x40,
+    // Maximum move command count
+    MU_COMMAND_MAX_COUNT = 0x40,
 
-	// Maximum size of uncompressed MU sprite sheet
-	MU_GFX_MAX_SIZE = 0x2200,
+    // Maximum size of uncompressed MU sprite sheet
+    MU_GFX_MAX_SIZE = 0x2200,
 
-	// Default base obj tile index for storing sprites in VRAM
-	MU_BASE_OBJ_TILE = 0x380,
+    // Default base obj tile index for storing sprites in VRAM
+    MU_BASE_OBJ_TILE = 0x380,
 
-	// obj palette id used for MU fade effects
-	MU_FADE_OBJ_PAL = 5,
+    // obj palette id used for MU fade effects
+    MU_FADE_OBJ_PAL = 5,
 
-	// Number of bits used to keep track of subpixels
-	MU_SUBPIXEL_PRECISION = 4,
+    // Number of bits used to keep track of subpixels
+    MU_SUBPIXEL_PRECISION = 4,
 };
 
 enum {
-	// Possible MU States
+    // Possible MU States
 
-	MU_STATE_NOSTATE,
-	MU_STATE_NONACTIVE,
-	MU_STATE_MOVEMENT,
-	MU_STATE_WAITING,
-	MU_STATE_UNK4,
-	MU_STATE_BUMPING,
-	MU_STATE_UI_DISPLAY,
-	MU_STATE_DEATHFADE,
+    MU_STATE_NOSTATE,
+    MU_STATE_NONACTIVE,
+    MU_STATE_MOVEMENT,
+    MU_STATE_WAITING,
+    MU_STATE_UNK4,
+    MU_STATE_BUMPING,
+    MU_STATE_UI_DISPLAY,
+    MU_STATE_DEATHFADE,
 };
 
 enum {
-	// MU command identifiers
+    // MU command identifiers
 
-	MU_COMMAND_END = -1, // end
+    MU_COMMAND_END = -1, // end
 
-	MU_COMMAND_MOVE_BASE,
+    MU_COMMAND_MOVE_BASE,
 
-	MU_COMMAND_MOVE_LEFT  = MU_COMMAND_MOVE_BASE + 0,
-	MU_COMMAND_MOVE_RIGHT = MU_COMMAND_MOVE_BASE + 1,
-	MU_COMMAND_MOVE_DOWN  = MU_COMMAND_MOVE_BASE + 2,
-	MU_COMMAND_MOVE_UP    = MU_COMMAND_MOVE_BASE + 3,
+    MU_COMMAND_MOVE_LEFT  = MU_COMMAND_MOVE_BASE + 0,
+    MU_COMMAND_MOVE_RIGHT = MU_COMMAND_MOVE_BASE + 1,
+    MU_COMMAND_MOVE_DOWN  = MU_COMMAND_MOVE_BASE + 2,
+    MU_COMMAND_MOVE_UP    = MU_COMMAND_MOVE_BASE + 3,
 
-	MU_COMMAND_HALT,
+    MU_COMMAND_HALT,
 
-	MU_COMMAND_FACE_BASE,
+    MU_COMMAND_FACE_BASE,
 
-	MU_COMMAND_FACE_LEFT  = MU_COMMAND_FACE_BASE + 0,
-	MU_COMMAND_FACE_RIGHT = MU_COMMAND_FACE_BASE + 1,
-	MU_COMMAND_FACE_DOWN  = MU_COMMAND_FACE_BASE + 2,
-	MU_COMMAND_FACE_UP    = MU_COMMAND_FACE_BASE + 3,
+    MU_COMMAND_FACE_LEFT  = MU_COMMAND_FACE_BASE + 0,
+    MU_COMMAND_FACE_RIGHT = MU_COMMAND_FACE_BASE + 1,
+    MU_COMMAND_FACE_DOWN  = MU_COMMAND_FACE_BASE + 2,
+    MU_COMMAND_FACE_UP    = MU_COMMAND_FACE_BASE + 3,
 
-	MU_COMMAND_WAIT,
-	MU_COMMAND_BUMP,
-	MU_COMMAND_UNK11,
-	MU_COMMAND_SET_SPEED,
+    MU_COMMAND_WAIT,
+    MU_COMMAND_BUMP,
+    MU_COMMAND_UNK11,
+    MU_COMMAND_SET_SPEED,
 
-	MU_COMMAND_CAMERA_ON,
-	MU_COMMAND_CAMERA_OFF,
+    MU_COMMAND_CAMERA_ON,
+    MU_COMMAND_CAMERA_OFF,
 };
 
 enum {
-	// MU flash identifiers
+    // MU flash identifiers
 
-	MU_FLASH_0,
-	MU_FLASH_1,
-	MU_FLASH_2,
-	MU_FLASH_3,
-	MU_FLASH_4,
-	MU_FLASH_5,
+    MU_FLASH_0,
+    MU_FLASH_1,
+    MU_FLASH_2,
+    MU_FLASH_3,
+    MU_FLASH_4,
+    MU_FLASH_5,
 };
 
 struct MUConfig;
 
 struct MUProc {
-	PROC_HEADER;
+    PROC_HEADER;
 
-	/* 2C */ struct Unit* pUnit;
-	/* 30 */ struct APHandle* pAPHandle;
-	/* 34 */ struct MUConfig* pMUConfig;
-	/* 38 */ void* pGfxVRAM;
+    /* 2C */ struct Unit* pUnit;
+    /* 30 */ struct APHandle* pAPHandle;
+    /* 34 */ struct MUConfig* pMUConfig;
+    /* 38 */ void* pGfxVRAM;
 
-	/* 3C */ u8 muIndex;
-	/* 3D */ u8 _u3D;
-	/* 3E */ u8 boolAttractCamera;
-	/* 3F */ u8 stateId;
-	/* 40 */ u8 boolIsHidden;
-	/* 41 */ u8 displayedClassId;
-	/* 42 */ s8 directionId;
-	/* 43 */ u8 stepSoundTimer;
-	/* 44 */ u8 boolForceMaxSpeed;
-	/* 46 */ u16 objPriorityBits;
-	/* 48 */ u16 moveTimer;
-	/* 4A */ short moveConfig;
+    /* 3C */ u8 muIndex;
+    /* 3D */ u8 _u3D;
+    /* 3E */ u8 boolAttractCamera;
+    /* 3F */ u8 stateId;
+    /* 40 */ u8 boolIsHidden;
+    /* 41 */ u8 displayedClassId;
+    /* 42 */ s8 directionId;
+    /* 43 */ u8 stepSoundTimer;
+    /* 44 */ u8 boolForceMaxSpeed;
+    /* 46 */ u16 objPriorityBits;
+    /* 48 */ u16 moveTimer;
+    /* 4A */ short moveConfig;
 
-	// Coordinates are in 16th of pixel
-	/* 4C */ short xSubPosition;
-	/* 4E */ short ySubPosition;
-	/* 50 */ short xSubOffset;
-	/* 52 */ short ySubOffset;
+    // Coordinates are in 16th of pixel
+    /* 4C */ short xSubPosition;
+    /* 4E */ short ySubPosition;
+    /* 50 */ short xSubOffset;
+    /* 52 */ short ySubOffset;
 };
 
 struct MUConfig {
-	/* 00 */ u8  muIndex;
-	/* 01 */ u8  paletteIndex;
-	/* 02 */ u16 objTileIndex;
-	/* 04 */ u8  currentCommand;
-	/* 05 */ s8  commands[MU_COMMAND_MAX_COUNT];
-	/* 45 */ // 3 byte padding
-	/* 48 */ struct MUProc* pMUProc;
+    /* 00 */ u8  muIndex;
+    /* 01 */ u8  paletteIndex;
+    /* 02 */ u16 objTileIndex;
+    /* 04 */ u8  currentCommand;
+    /* 05 */ s8  commands[MU_COMMAND_MAX_COUNT];
+    /* 45 */ // 3 byte padding
+    /* 48 */ struct MUProc* pMUProc;
 };
 
 struct MMSData {
-	const void* pGraphics;
-	const void* pAnimation;
+    const void* pGraphics;
+    const void* pAnimation;
 };
 
 struct PositionS16 {
-	short x, y;
+    short x, y;
 };
 
 extern const struct ProcCmd gProcScr_MoveUnit[];
