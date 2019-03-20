ba9e81b9aeffe7cad484a7a2c2fe472e032f7f70
Stan H <deindall@gmail.com>
2019-03-20 16:31:48 +0100

    (animedrv) cleanup

--- src/animedrv.c
+++ src/animedrv.c
@@ -1,34 +1,34 @@
 #include "global.h"
 
-struct AnimationInterpreter
+struct Anim
 {
     /* 00 */ u16 state;
     /* 02 */ short xPosition;
     /* 04 */ short yPosition;
-    /* 06 */ short delayTimer;
-    /* 08 */ u16 oam2base;
+    /* 06 */ short timer;
+    /* 08 */ u16 oam2Base;
     /* 0A */ u16 drawLayerPriority;
     /* 0C */ u16 state2;
     /* 0E */ u16 nextRoundId;
     /* 10 */ u16 state3;
     /* 12 */ u8 currentRoundType;
-    /* 13 */ u8 frameIndex;
+    /* 13 */ u8 unk13;
 
-    /* 14 */ u8 queuedCommandCount;
+    /* 14 */ u8 commandQueueSize;
     /* 15 */ u8 commandQueue[7];
 
     /* 1C */ u32 oamBase;
 
-    /* 20 */ const u32* pCurrentFrame;
-    /* 24 */ const u32* pStartFrame;
-    /* 28 */ const void* pUnk28;
+    /* 20 */ const u32* pScrCurrent;
+    /* 24 */ const u32* pScrStart;
+    /* 28 */ const void* pImgSheet;
     /* 2C */ const void* pUnk2C;
-    /* 30 */ const void* pStartObjData; // aka "OAM data"
+    /* 30 */ const void* pSpriteDataPool; // aka "OAM data"
 
-    /* 34 */ struct AnimationInterpreter* pPrev;
-    /* 38 */ struct AnimationInterpreter* pNext;
+    /* 34 */ struct Anim* pPrev;
+    /* 38 */ struct Anim* pNext;
 
-    /* 3C */ const void* pUnk3C;
+    /* 3C */ const void* pSpriteData;
     /* 40 */ const void* pUnk40;
     /* 44 */ const void* pUnk44;
 };
@@ -60,11 +60,11 @@ struct AnimSpriteData
 
 enum
 {
-    ANIM_BIT_0 = (1 << 0),
-    ANIM_BIT_1 = (1 << 1),
-    ANIM_BIT_2 = (1 << 2),
-    ANIM_BIT_3 = (1 << 3),
-    ANIM_BIT_4 = (1 << 4),
+    // For use with 
+    ANIM_BIT_ENABLED = (1 << 0),
+    ANIM_BIT_HIDDEN  = (1 << 1),
+    ANIM_BIT_2       = (1 << 2),
+    ANIM_BIT_FROZEN  = (1 << 3),
 };
 
 enum
@@ -72,26 +72,81 @@ enum
     ANIM_MAX_COUNT = 50,
 };
 
-#define ANIM_IS_DISABLED(ais) ((ais)->state == 0)
+enum
+{
+    // Animation Command Identifiers
+
+    // TODO: complete during battle animation decomp
+
+    ANIM_CMD_NOP     = 0x00,
+    ANIM_CMD_WAIT_01 = 0x01, // wait for hp depletion
+    ANIM_CMD_WAIT_02 = 0x02, // wait for dodge start, then dodge
+    ANIM_CMD_WAIT_03 = 0x03, // wait for attack start
+    ANIM_CMD_WAIT_04 = 0x04,
+    ANIM_CMD_WAIT_05 = 0x05, // wait for spell animation?
+    // TODO: more
+    ANIM_CMD_WAIT_13 = 0x13, // unk
+    ANIM_CMD_WAIT_18 = 0x18, // wait for dodge start, then forward dodge
+    ANIM_CMD_WAIT_2D = 0x2D, // unk
+    ANIM_CMD_WAIT_39 = 0x39, // unk
+    ANIM_CMD_WAIT_52 = 0x52, // unk
+};
+
+#define ANIM_IS_DISABLED(anim) ((anim)->state == 0)
+
+#define ANINS_IS_NOT_FORCESPRITE(instruction) ((instruction) & 0x80000000)
+#define ANINS_IS_PTRINS(instruction) ((instruction) & 0x40000000)
+
+#define ANINS_FORCESPRITE_GET_ADDRESS(instruction) ((void*) ((instruction) &~ 0xF0000003))
+#define ANINS_FORCESPRITE_GET_DELAY(instruction) ((((instruction) >> 26) & 0x1C) + ((instruction) & 3))
+
+#define ANINS_PTRINS_GET_TYPE(instruction) (0x3 & ((instruction) >> 28))
+#define ANINS_PTRINS_GET_ADDRESS(instruction) ((void*) ((instruction) &~ 0xF0000000))
+
+#define ANINS_GET_TYPE(instruction) (0x3F & ((instruction) >> 24))
+
+#define ANINS_WAIT_GET_DELAY(instruction) ((instruction) & 0xFFFF)
+
+#define ANINS_MOVE_GET_XOFF(instruction) (((int) ((instruction) << 24)) >> 24)
+#define ANINS_MOVE_GET_YOFF(instruction) (((int) ((instruction) << 16)) >> 24)
+#define ANINS_MOVE_GET_DELAY(instruction) (((instruction) >> 16) & 0xFF)
+
+#define ANINS_COMMAND_GET_ID(instruction) (0xFF & (instruction))
+
+#define ANINS_FRAME_GET_DELAY(instruction) ((instruction) & 0xFFFF)
+#define ANINS_FRAME_GET_UNK(instruction) ((instruction) >> 16) & 0xFF
+
+enum
+{
+    ANIM_INS_TYPE_STOP    = 0,
+    ANIM_INS_TYPE_END     = 1,
+    ANIM_INS_TYPE_LOOP    = 2,
+    ANIM_INS_TYPE_MOVE    = 3,
+    ANIM_INS_TYPE_WAIT    = 4,
+    ANIM_INS_TYPE_COMMAND = 5,
+    ANIM_INS_TYPE_FRAME   = 6,
+};
+
+typedef void (*AnimCallback_t) (struct Anim* anim);
 
 void AIS_ExecAll(void);
 void ClearAISArray(void);
-struct AnimationInterpreter* sub_8004EE8(const void* frameData);
-struct AnimationInterpreter* AIS_New(const void* frameData, u16 displayPriority);
+struct Anim* sub_8004EE8(const void* frameData);
+struct Anim* AIS_New(const void* frameData, u16 displayPriority);
 void AISArray_Sort(void);
-void AIS_Free(struct AnimationInterpreter* anim);
-void AIS_Display(struct AnimationInterpreter* anim);
-int HandleNextAISFrame(struct AnimationInterpreter* anim);
-void LinkAIS(struct AnimationInterpreter* anim);
-void _AIS_Display(struct AnimationInterpreter* anim);
-void sub_8005334(struct AnimationInterpreter* anim, u32 instruction);
+void AIS_Free(struct Anim* anim);
+void AIS_Display(struct Anim* anim);
+int HandleNextAISFrame(struct Anim* anim);
+void LinkAIS(struct Anim* anim);
+void _AIS_Display(struct Anim* anim);
+void sub_8005334(struct Anim* anim, u32 instruction);
 
-EWRAM_DATA static struct AnimationInterpreter gUnknown_02028F78[ANIM_MAX_COUNT] = {};
-EWRAM_DATA static struct AnimationInterpreter* gUnknown_02029D88 = NULL;
+EWRAM_DATA static struct Anim gUnknown_02028F78[ANIM_MAX_COUNT] = {};
+EWRAM_DATA static struct Anim* gUnknown_02029D88 = NULL;
 
 void AIS_ExecAll(void)
 {
-    struct AnimationInterpreter* it;
+    struct Anim* it;
     int boolNeedsSort = FALSE;
 
     if (!gUnknown_02029D88)
@@ -102,23 +157,23 @@ void AIS_ExecAll(void)
         if (ANIM_IS_DISABLED(it))
             continue;
 
-        if (!(it->state & ANIM_BIT_3))
+        if (!(it->state & ANIM_BIT_FROZEN))
         {
-            if (it->delayTimer == 0 || --it->delayTimer == 0)
+            if (it->timer == 0 || --it->timer == 0)
             {
                 do
                 {
                     if (HandleNextAISFrame(it) == TRUE)
                         boolNeedsSort = TRUE;
                 }
-                while (it->delayTimer == 0);
+                while (it->timer == 0);
             }
 
             if (ANIM_IS_DISABLED(it))
                 continue;
         }
 
-        if (!(it->state & ANIM_BIT_1))
+        if (!(it->state & ANIM_BIT_HIDDEN))
             _AIS_Display(it);
 
         if (!it->pNext)
@@ -131,7 +186,7 @@ void AIS_ExecAll(void)
 
 void ClearAISArray(void)
 {
-    struct AnimationInterpreter* it;
+    struct Anim* it;
 
     for (it = gUnknown_02028F78; it < gUnknown_02028F78 + ANIM_MAX_COUNT; ++it)
     {
@@ -143,9 +198,9 @@ void ClearAISArray(void)
     gUnknown_02029D88 = NULL;
 }
 
-struct AnimationInterpreter* sub_8004EE8(const void* frameData)
+struct Anim* sub_8004EE8(const void* frameData)
 {
-    struct AnimationInterpreter* anim;
+    struct Anim* anim;
 
     // Find anim slot for new anim
     for (anim = gUnknown_02028F78; (anim < gUnknown_02028F78 + ANIM_MAX_COUNT) && !ANIM_IS_DISABLED(anim); ++anim) {}
@@ -154,22 +209,22 @@ struct AnimationInterpreter* sub_8004EE8(const void* frameData)
     if (anim == gUnknown_02028F78 + ANIM_MAX_COUNT)
         return NULL;
 
-    anim->state = ANIM_BIT_0;
+    anim->state = ANIM_BIT_ENABLED;
 
-    anim->pCurrentFrame = frameData;
-    anim->pStartFrame = frameData;
+    anim->pScrCurrent = frameData;
+    anim->pScrStart = frameData;
 
-    anim->delayTimer = 0;
-    anim->oam2base = 0;
+    anim->timer = 0;
+    anim->oam2Base = 0;
     anim->drawLayerPriority = 0;
     anim->state2 = 0;
     anim->state3 = 0;
     anim->oamBase = 0;
 
-    anim->queuedCommandCount = 0;
+    anim->commandQueueSize = 0;
 
     anim->pUnk2C = NULL;
-    anim->pStartObjData = NULL;
+    anim->pSpriteDataPool = NULL;
     anim->pUnk40 = NULL;
     anim->pUnk44 = NULL;
 
@@ -178,9 +233,9 @@ struct AnimationInterpreter* sub_8004EE8(const void* frameData)
     return anim;
 }
 
-struct AnimationInterpreter* AIS_New(const void* frameData, u16 displayPriority)
+struct Anim* AIS_New(const void* frameData, u16 displayPriority)
 {
-    struct AnimationInterpreter* anim;
+    struct Anim* anim;
 
     // Find anim slot for new anim
     for (anim = gUnknown_02028F78; (anim < gUnknown_02028F78 + ANIM_MAX_COUNT) && !ANIM_IS_DISABLED(anim); ++anim) {}
@@ -189,22 +244,22 @@ struct AnimationInterpreter* AIS_New(const void* frameData, u16 displayPriority)
     if (anim == gUnknown_02028F78 + ANIM_MAX_COUNT)
         return NULL;
 
-    anim->state = ANIM_BIT_0;
+    anim->state = ANIM_BIT_ENABLED;
 
-    anim->pCurrentFrame = frameData;
-    anim->pStartFrame = frameData;
+    anim->pScrCurrent = frameData;
+    anim->pScrStart = frameData;
 
-    anim->delayTimer = 0;
-    anim->oam2base = 0;
+    anim->timer = 0;
+    anim->oam2Base = 0;
     anim->drawLayerPriority = displayPriority;
     anim->state2 = 0;
     anim->state3 = 0;
     anim->oamBase = 0;
 
-    anim->queuedCommandCount = 0;
+    anim->commandQueueSize = 0;
 
     anim->pUnk2C = NULL;
-    anim->pStartObjData = NULL;
+    anim->pSpriteDataPool = NULL;
     anim->pUnk40 = NULL;
     anim->pUnk44 = NULL;
 
@@ -215,7 +270,7 @@ struct AnimationInterpreter* AIS_New(const void* frameData, u16 displayPriority)
 
 void AISArray_Sort(void)
 {
-    struct AnimationInterpreter* anim;
+    struct Anim* anim;
 
     for (anim = gUnknown_02028F78; anim < gUnknown_02028F78 + ANIM_MAX_COUNT; ++anim)
     {
@@ -237,7 +292,7 @@ void AISArray_Sort(void)
     }
 }
 
-void AIS_Free(struct AnimationInterpreter* anim)
+void AIS_Free(struct Anim* anim)
 {
     if (anim->pPrev == NULL)
     {
@@ -255,126 +310,114 @@ void AIS_Free(struct AnimationInterpreter* anim)
     anim->pNext = NULL;
 }
 
-void AIS_Display(struct AnimationInterpreter* anim)
+void AIS_Display(struct Anim* anim)
 {
     _AIS_Display(anim);
 }
 
-int HandleNextAISFrame(struct AnimationInterpreter* anim)
+int HandleNextAISFrame(struct Anim* anim)
 {
-    // TODO: clean anim mess up
-
     int boolNeedsResort = FALSE;
 
-    u32 instruction = *anim->pCurrentFrame++;
+    u32 instruction = *anim->pScrCurrent++;
 
-    if (instruction & 0x80000000)
+    if (ANINS_IS_NOT_FORCESPRITE(instruction))
     {
-        if (instruction & 0x40000000)
+        if (ANINS_IS_PTRINS(instruction))
         {
-            switch (0x3 & (instruction >> 28))
+            switch (ANINS_PTRINS_GET_TYPE(instruction))
             {
 
             case 0: // function call
-            {
-                void(*func)(struct AnimationInterpreter* anim) =
-                    (void(*)(struct AnimationInterpreter* anim)) (instruction &~ 0xF0000000);
-
-                func(anim);
+                ((AnimCallback_t) (ANINS_PTRINS_GET_ADDRESS(instruction)))(anim);
                 break;
-            }
 
             case 1: // set new frame data
-            {
-                const void* frame =
-                    (const void*) (instruction &~ 0xF0000000);
+                anim->pScrStart   = ANINS_PTRINS_GET_ADDRESS(instruction);
+                anim->pScrCurrent = ANINS_PTRINS_GET_ADDRESS(instruction);
 
-                anim->pStartFrame = frame;
-                anim->pCurrentFrame = frame;
-                anim->delayTimer = 1;
+                anim->timer = 1;
 
                 break;
-            }
 
-            } // switch (0x3 & (instruction >> 28))
+            } // switch (ANINS_PTRINS_GET_TYPE(instruction))
         }
         else
         {
-            switch (0x3F & (instruction >> 24))
+            switch (ANINS_GET_TYPE(instruction))
             {
 
-            case 0:
-                anim->pCurrentFrame--;
-                anim->delayTimer = 1;
+            case ANIM_INS_TYPE_STOP:
+                anim->pScrCurrent--;
+                anim->timer = 1;
 
                 anim->state2 = (anim->state2 & 0xFFF) | 0x4000;
 
                 break;
 
-            case 1:
+            case ANIM_INS_TYPE_END:
                 anim->state = 0;
-                anim->delayTimer = 1;
+                anim->timer = 1;
 
                 boolNeedsResort = TRUE;
 
                 break;
 
-            case 2:
-                anim->pCurrentFrame = anim->pStartFrame;
-                anim->delayTimer = 1;
+            case ANIM_INS_TYPE_LOOP:
+                anim->pScrCurrent = anim->pScrStart;
+                anim->timer = 1;
 
                 break;
 
-            case 4:
-                anim->delayTimer = instruction;
+            case ANIM_INS_TYPE_WAIT:
+                anim->timer = ANINS_WAIT_GET_DELAY(instruction);
                 break;
 
-            case 3:
-                anim->xPosition += ((int) (instruction << 24)) >> 24;
-                anim->yPosition += ((int) (instruction << 16)) >> 24;
-                anim->delayTimer = (instruction >> 16) & 0xFF;
+            case ANIM_INS_TYPE_MOVE:
+                anim->xPosition += ANINS_MOVE_GET_XOFF(instruction);
+                anim->yPosition += ANINS_MOVE_GET_YOFF(instruction);
+
+                anim->timer = ANINS_MOVE_GET_DELAY(instruction);
 
                 break;
 
-            case 5:
+            case ANIM_INS_TYPE_COMMAND:
                 anim->state2 = (anim->state2 & 0xFFF) | 0x1000;
 
-                anim->commandQueue[anim->queuedCommandCount] = instruction;
-                anim->queuedCommandCount++;
+                anim->commandQueue[anim->commandQueueSize] = ANINS_COMMAND_GET_ID(instruction);
+                anim->commandQueueSize++;
 
-                anim->delayTimer = 1;
+                anim->timer = 1;
 
-                switch (instruction & 0xFF)
+                switch (ANINS_COMMAND_GET_ID(instruction))
                 {
 
-                    // TODO: BATTLE ANIMATION COMMAND DEFINITIONS
-
-                case 1:
-                case 2:
-                case 3:
-                case 4:
-                case 5:
-                case 0x13:
-                case 0x18:
-                case 0x2D:
-                case 0x39:
-                case 0x52:
-                    anim->pCurrentFrame--;
+                case ANIM_CMD_WAIT_01:
+                case ANIM_CMD_WAIT_02:
+                case ANIM_CMD_WAIT_03:
+                case ANIM_CMD_WAIT_04:
+                case ANIM_CMD_WAIT_05:
+                case ANIM_CMD_WAIT_13:
+                case ANIM_CMD_WAIT_18:
+                case ANIM_CMD_WAIT_2D:
+                case ANIM_CMD_WAIT_39:
+                case ANIM_CMD_WAIT_52:
+                    anim->pScrCurrent--;
                     break;
 
-                } // switch (instruction & 0xFF)
+                } // switch (ANINS_COMMAND_GET_ID(instruction))
 
                 break;
 
-            case 6:
-                anim->delayTimer = instruction;
-                anim->frameIndex = instruction >> 16;
+            case ANIM_INS_TYPE_FRAME:
+                anim->timer = ANINS_FRAME_GET_DELAY(instruction);
+                anim->unk13 = ANINS_FRAME_GET_UNK(instruction);
 
-                anim->pUnk28 = (const void*) (*anim->pCurrentFrame++);
+                anim->pImgSheet = (const void*) (*anim->pScrCurrent++);
 
                 // ???????
-                anim->pUnk3C = (const void*) (*anim->pCurrentFrame++);
-                anim->pUnk3C += (unsigned) anim->pStartObjData;
+                anim->pSpriteData = (const void*) (*anim->pScrCurrent++);
+                anim->pSpriteData += (unsigned) anim->pSpriteDataPool;
 
                 anim->state2 = (anim->state2 & 0xFFF) | 0x2000;
 
@@ -385,16 +428,16 @@ int HandleNextAISFrame(struct AnimationInterpreter* anim)
     }
     else
     {
-        anim->pUnk3C     = (const void*) (instruction &~ 0xF0000003);
-        anim->delayTimer = ((instruction >> 26) & 0x1C) + (instruction & 3);
+        anim->pSpriteData = ANINS_FORCESPRITE_GET_ADDRESS(instruction);
+        anim->timer       = ANINS_FORCESPRITE_GET_DELAY(instruction);
     }
 
     return boolNeedsResort;
 }
 
-void LinkAIS(struct AnimationInterpreter* anim)
+void LinkAIS(struct Anim* anim)
 {
-    struct AnimationInterpreter* it = gUnknown_02029D88;
+    struct Anim* it = gUnknown_02029D88;
 
     if (!gUnknown_02029D88)
     {
@@ -406,7 +449,7 @@ void LinkAIS(struct AnimationInterpreter* anim)
     {
         if (anim->drawLayerPriority > it->drawLayerPriority)
         {
-            struct AnimationInterpreter* prev = it->pPrev;
+            struct Anim* prev = it->pPrev;
 
             anim->pPrev = it->pPrev;
             anim->pNext = it;
@@ -433,11 +476,11 @@ void LinkAIS(struct AnimationInterpreter* anim)
     }
 }
 
-void _AIS_Display(struct AnimationInterpreter* anim)
+void _AIS_Display(struct Anim* anim)
 {
     unsigned baseAffineId = gUnknown_0300312C;
 
-    const struct AnimSpriteData* oamData = anim->pUnk3C;
+    const struct AnimSpriteData* oamData = anim->pSpriteData;
     const struct AnimSpriteData* it;
 
     int i;
@@ -491,29 +534,27 @@ void _AIS_Display(struct AnimationInterpreter* anim)
 
         // ugh
         *gUnknown_03003744++ = (it->header + i) | (x << 16) | (y);
-        *(u16*)(gUnknown_03003744++) = (it->as.object.oam2 & 0xF3FF) + anim->oam2base;
+        *(u16*)(gUnknown_03003744++) = (it->as.object.oam2 & 0xF3FF) + anim->oam2Base;
     }
 }
 
-void sub_8005334(struct AnimationInterpreter* anim, u32 instruction)
+void sub_8005334(struct Anim* anim, u32 instruction)
 {
-    switch (instruction & 0xFF)
+    switch (ANINS_COMMAND_GET_ID(instruction))
     {
 
-        // TODO: BATTLE ANIMATION COMMAND DEFINITIONS
-
-    case 1:
-    case 2:
-    case 3:
-    case 4:
-    case 5:
-    case 0x13:
-    case 0x18:
-    case 0x2D:
-    case 0x39:
-    case 0x52:
-        anim->pCurrentFrame--;
+    case ANIM_CMD_WAIT_01:
+    case ANIM_CMD_WAIT_02:
+    case ANIM_CMD_WAIT_03:
+    case ANIM_CMD_WAIT_04:
+    case ANIM_CMD_WAIT_05:
+    case ANIM_CMD_WAIT_13:
+    case ANIM_CMD_WAIT_18:
+    case ANIM_CMD_WAIT_2D:
+    case ANIM_CMD_WAIT_39:
+    case ANIM_CMD_WAIT_52:
+        anim->pScrCurrent--;
         break;
 
-    } // switch (instruction & 0xFF)
+    } // switch (ANINS_COMMAND_GET_ID(instruction))
 }
