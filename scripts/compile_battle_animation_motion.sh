b7f651c95c47e2559bfd296984323ee84602f003
laqieer <laqieer@126.com>
2019-05-02 03:02:38 +0000

    Enhancement: replace gcc with as when compiling banim

--- scripts/compile_battle_animation_motion.sh
+++ scripts/compile_battle_animation_motion.sh
@@ -7,7 +7,7 @@ cd banim
 ls | grep '_motion.s' | while read f_motion
 do
     f_base=${f_motion%motion.s}
-    arm-none-eabi-gcc $f_motion -c
+    arm-none-eabi-as $f_motion -o $f_base'motion.o'
     arm-none-eabi-objcopy -O binary --dump-section .data.modes=$f_base"modes.bin" --dump-section .data.script=$f_base"script.bin" --dump-section .data.oam_r=$f_base"oam_r.bin" --dump-section .data.oam_l=$f_base"oam_l.bin" $f_base"motion.o" $f_base"motion.bin"
     ../tools/gbagfx/gbagfx $f_base"script.bin" $f_base"script.bin.lz"
     ../tools/gbagfx/gbagfx $f_base"oam_r.bin" $f_base"oam_r.bin.lz"
