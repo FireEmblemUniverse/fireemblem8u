9c3c3b1d75cfa62d0affb2862897353cf3dc2d57
ProjectRevoTPP <projectrevotpp@hotmail.com>
2019-05-04 18:05:40 -0400

    formatting

--- data/data_599714.s
+++ data/data_599714.s
@@ -1,165 +1,165 @@
-	.section .data
+    .section .data
 
     .global gUnknown_08599714
 gUnknown_08599714:  @ 0x08599714
-	.incbin "baserom.gba", 0x599714, 0x20
+    .incbin "baserom.gba", 0x599714, 0x20
 
     .global gUnknown_08599734
 gUnknown_08599734:  @ 0x08599734
-	.incbin "baserom.gba", 0x599734, 0x200
+    .incbin "baserom.gba", 0x599734, 0x200
 
     .global gUnknown_08599934
 gUnknown_08599934:  @ 0x08599934
-	.incbin "baserom.gba", 0x599934, 0x200
+    .incbin "baserom.gba", 0x599934, 0x200
 
     .global gUnknown_08599B34
 gUnknown_08599B34:  @ 0x08599B34
-	.incbin "baserom.gba", 0x599B34, 0x200
+    .incbin "baserom.gba", 0x599B34, 0x200
 
     .global gUnknown_08599D34
 gUnknown_08599D34:  @ 0x08599D34
-	.incbin "baserom.gba", 0x599D34, 0x200
+    .incbin "baserom.gba", 0x599D34, 0x200
 
     .global gUnknown_08599F34
 gUnknown_08599F34:  @ 0x08599F34
-	.incbin "baserom.gba", 0x599F34, 0x20
+    .incbin "baserom.gba", 0x599F34, 0x20
 
     .global gUnknown_08599F54
 gUnknown_08599F54:  @ 0x08599F54
-	.incbin "baserom.gba", 0x599F54, 0x20
+    .incbin "baserom.gba", 0x599F54, 0x20
 
     .global gUnknown_08599F74
 gUnknown_08599F74:  @ 0x08599F74
-	.incbin "baserom.gba", 0x599F74, 0x40
+    .incbin "baserom.gba", 0x599F74, 0x40
 
-	.global gUnknown_08599FB4
+    .global gUnknown_08599FB4
 gUnknown_08599FB4:  @ 0x08599FB4
-	.incbin "baserom.gba", 0x599FB4, 0x20
+    .incbin "baserom.gba", 0x599FB4, 0x20
 
-	.global gUnknown_08599FD4
+    .global gUnknown_08599FD4
 gUnknown_08599FD4:  @ 0x08599FD4
-	.incbin "baserom.gba", 0x599FD4, 0x20
+    .incbin "baserom.gba", 0x599FD4, 0x20
 
-	.global gUnknown_08599FF4
+    .global gUnknown_08599FF4
 gUnknown_08599FF4:  @ 0x08599FF4
-	.incbin "baserom.gba", 0x599FF4, 0x18
+    .incbin "baserom.gba", 0x599FF4, 0x18
 
-	.global gUnknown_0859A00C
+    .global gUnknown_0859A00C
 gUnknown_0859A00C:  @ 0x0859A00C
-	.incbin "baserom.gba", 0x59A00C, 0x18
+    .incbin "baserom.gba", 0x59A00C, 0x18
 
-	.global gUnknown_0859A024
+    .global gUnknown_0859A024
 gUnknown_0859A024:  @ 0x0859A024
-	.incbin "baserom.gba", 0x59A024, 0x20
+    .incbin "baserom.gba", 0x59A024, 0x20
 
-	.global gUnknown_0859A044
+    .global gUnknown_0859A044
 gUnknown_0859A044:  @ 0x0859A044
-	.incbin "baserom.gba", 0x59A044, 0x20
+    .incbin "baserom.gba", 0x59A044, 0x20
 
-	.global gUnknown_0859A064
+    .global gUnknown_0859A064
 gUnknown_0859A064:  @ 0x0859A064
-	.incbin "baserom.gba", 0x59A064, 0x20
+    .incbin "baserom.gba", 0x59A064, 0x20
 
-	.global gUnknown_0859A084
+    .global gUnknown_0859A084
 gUnknown_0859A084:  @ 0x0859A084
-	.incbin "baserom.gba", 0x59A084, 0x20
+    .incbin "baserom.gba", 0x59A084, 0x20
 
-	.global gUnknown_0859A0A4
+    .global gUnknown_0859A0A4
 gUnknown_0859A0A4:  @ 0x0859A0A4
-	.incbin "baserom.gba", 0x59A0A4, 0x30
+    .incbin "baserom.gba", 0x59A0A4, 0x30
 
-	.global gUnknown_0859A0D4
+    .global gUnknown_0859A0D4
 gUnknown_0859A0D4:  @ 0x0859A0D4
-	.incbin "baserom.gba", 0x59A0D4, 0x18
+    .incbin "baserom.gba", 0x59A0D4, 0x18
 
-	.global gUnknown_0859A0EC
+    .global gUnknown_0859A0EC
 gUnknown_0859A0EC:  @ 0x0859A0EC
-	.incbin "baserom.gba", 0x59A0EC, 0x4
+    .incbin "baserom.gba", 0x59A0EC, 0x4
 
-	.global gUnknown_0859A0F0
+    .global gUnknown_0859A0F0
 gUnknown_0859A0F0:  @ 0x0859A0F0
-	.incbin "baserom.gba", 0x59A0F0, 0x3
+    .incbin "baserom.gba", 0x59A0F0, 0x3
 
-	.global gUnknown_0859A0F3
+    .global gUnknown_0859A0F3
 gUnknown_0859A0F3:  @ 0x0859A0F3
-	.incbin "baserom.gba", 0x59A0F3, 0x1
+    .incbin "baserom.gba", 0x59A0F3, 0x1
 
-	.global gUnknown_0859A0F4
+    .global gUnknown_0859A0F4
 gUnknown_0859A0F4:  @ 0x0859A0F4
-	.incbin "baserom.gba", 0x59A0F4, 0x4
+    .incbin "baserom.gba", 0x59A0F4, 0x4
 
-	.global gUnknown_0859A0F8
+    .global gUnknown_0859A0F8
 gUnknown_0859A0F8:  @ 0x0859A0F8
-	.incbin "baserom.gba", 0x59A0F8, 0x8
+    .incbin "baserom.gba", 0x59A0F8, 0x8
 
-	.global gUnknown_0859A100
+    .global gUnknown_0859A100
 gUnknown_0859A100:  @ 0x0859A100
-	.incbin "baserom.gba", 0x59A100, 0x10
+    .incbin "baserom.gba", 0x59A100, 0x10
 
-	.global gUnknown_0859A110
+    .global gUnknown_0859A110
 gUnknown_0859A110:  @ 0x0859A110
-	.incbin "baserom.gba", 0x59A110, 0x10
+    .incbin "baserom.gba", 0x59A110, 0x10
 
-	.global gUnknown_0859A120
+    .global gUnknown_0859A120
 gUnknown_0859A120:  @ 0x0859A120
-	.incbin "baserom.gba", 0x59A120, 0x20
+    .incbin "baserom.gba", 0x59A120, 0x20
 
-	.global gUnknown_0859A140
+    .global gUnknown_0859A140
 gUnknown_0859A140:  @ 0x0859A140
-	.incbin "baserom.gba", 0x59A140, 0x20
+    .incbin "baserom.gba", 0x59A140, 0x20
 
-	.global gUnknown_0859A160
+    .global gUnknown_0859A160
 gUnknown_0859A160:  @ 0x0859A160
-	.incbin "baserom.gba", 0x59A160, 0x20
+    .incbin "baserom.gba", 0x59A160, 0x20
 
-	.global gUnknown_0859A180
+    .global gUnknown_0859A180
 gUnknown_0859A180:  @ 0x0859A180
-	.incbin "baserom.gba", 0x59A180, 0x20
+    .incbin "baserom.gba", 0x59A180, 0x20
 
-	.global gUnknown_0859A1A0
+    .global gUnknown_0859A1A0
 gUnknown_0859A1A0:  @ 0x0859A1A0
-	.incbin "baserom.gba", 0x59A1A0, 0x20
+    .incbin "baserom.gba", 0x59A1A0, 0x20
 
-	.global gUnknown_0859A1C0
+    .global gUnknown_0859A1C0
 gUnknown_0859A1C0:  @ 0x0859A1C0
-	.incbin "baserom.gba", 0x59A1C0, 0x20
+    .incbin "baserom.gba", 0x59A1C0, 0x20
 
-	.global gUnknown_0859A1E0
+    .global gUnknown_0859A1E0
 gUnknown_0859A1E0:  @ 0x0859A1E0
-	.incbin "baserom.gba", 0x59A1E0, 0x10
+    .incbin "baserom.gba", 0x59A1E0, 0x10
 
-	.global gProc_BMapMain
+    .global gProc_BMapMain
 gProc_BMapMain:  @ 0x0859A1F0
-	.incbin "baserom.gba", 0x59A1F0, 0x248
+    .incbin "baserom.gba", 0x59A1F0, 0x248
 
-	.global gUnknown_0859A438
+    .global gUnknown_0859A438
 gUnknown_0859A438:  @ 0x0859A438
-	.incbin "baserom.gba", 0x59A438, 0x6E
+    .incbin "baserom.gba", 0x59A438, 0x6E
 
-	.global gUnknown_0859A4A6
+    .global gUnknown_0859A4A6
 gUnknown_0859A4A6:  @ 0x0859A4A6
-	.incbin "baserom.gba", 0x59A4A6, 0x1A
+    .incbin "baserom.gba", 0x59A4A6, 0x1A
 
-	.global gUnknown_0859A4C0
+    .global gUnknown_0859A4C0
 gUnknown_0859A4C0:  @ 0x0859A4C0
-	.incbin "baserom.gba", 0x59A4C0, 0x70
+    .incbin "baserom.gba", 0x59A4C0, 0x70
 
-	.global gUnknown_0859A530
+    .global gUnknown_0859A530
 gUnknown_0859A530:  @ 0x0859A530
-	.incbin "baserom.gba", 0x59A530, 0xC
+    .incbin "baserom.gba", 0x59A530, 0xC
 
-	.global gUnknown_0859A53C
+    .global gUnknown_0859A53C
 gUnknown_0859A53C:  @ 0x0859A53C
-	.incbin "baserom.gba", 0x59A53C, 0xC
+    .incbin "baserom.gba", 0x59A53C, 0xC
 
-	.global gUnknown_0859A548
+    .global gUnknown_0859A548
 gUnknown_0859A548:  @ 0x0859A548
-	.incbin "baserom.gba", 0x59A548, 0x28
+    .incbin "baserom.gba", 0x59A548, 0x28
 
-	.global gUnknown_0859A570
+    .global gUnknown_0859A570
 gUnknown_0859A570:  @ 0x0859A570
-	.incbin "baserom.gba", 0x59A570, 0x10
+    .incbin "baserom.gba", 0x59A570, 0x10
 
-	.global gUnknown_0859A580
+    .global gUnknown_0859A580
 gUnknown_0859A580:  @ 0x0859A580
-	.incbin "baserom.gba", 0x59A580, 0x18
+    .incbin "baserom.gba", 0x59A580, 0x18
--- data/face_rodata.s
+++ data/face_rodata.s
@@ -1,13 +1,13 @@
-	.section .rodata
+    .section .rodata
 
-	.align 2, 0
+    .align 2, 0
 
-	.global gUnknown_080D77B4
+    .global gUnknown_080D77B4
 gUnknown_080D77B4:  @ 0x080D77B4
-	.byte 0x45, 0x5F, 0x46, 0x41, 0x43, 0x45
+    .byte 0x45, 0x5F, 0x46, 0x41, 0x43, 0x45
 
     .align 2
-	.global gUnknown_080D77BC
+    .global gUnknown_080D77BC
 gUnknown_080D77BC:  @ 0x080D77BC
     .word gUnknown_08599D34
     .word gUnknown_08599B34
@@ -18,7 +18,7 @@ gUnknown_080D77BC:  @ 0x080D77BC
     .word gUnknown_08599934
     .word gUnknown_08599D34
 
-	.global gUnknown_080D77DC
+    .global gUnknown_080D77DC
 gUnknown_080D77DC:  @ 0x080D77DC
     .word gUnknown_08599F34
     .word gUnknown_08599F54
