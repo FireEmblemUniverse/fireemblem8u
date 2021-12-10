bf972106af834c068ea5429ee694f0f52e1dce02
Noah Nelson <nelson.noah96@gmail.com>
2021-12-10 02:40:27 +0000

    Clean some things

--- src/mapanim.c
+++ src/mapanim.c
@@ -13,53 +13,54 @@
 
 #include "mapanim.h"
 
-void unused_sub_807A708(u32 a) {
+// unreferenced
+void sub_807A708(u32 a) {
     if (gUnknown_0203E1F0.u62 == 1) {
         sub_8011694(gUnknown_0203E1F0.actors[1].pBattleUnit->weapon, a);
     }
 }
 
 void DisplayWpnBrokePopup(u32 a) {
-    struct BattleUnit * something = 0;
+    struct BattleUnit *unit = 0;
     if (BattleUnit_ShouldDisplayWpnBroke(&gBattleActor)) {
-        something = &gBattleActor;
+        unit = &gBattleActor;
     }
     if (BattleUnit_ShouldDisplayWpnBroke(&gBattleTarget)) {
-        something = &gBattleTarget;
+        unit = &gBattleTarget;
     }
-    if (something) {
-        NewPopup_WeaponBroke(something->weaponBefore, a);
+    if (unit) {
+        NewPopup_WeaponBroke(unit->weaponBefore, a);
     }
 }
 
 s8 BattleUnit_ShouldDisplayWpnBroke(struct BattleUnit *u) {
     u32 result;
-    if ((u->unit.index & 0xc0))
-        result = 0; else
+    if (u->unit.index & 0xc0)
+        result = 0;
+    else
         result = DidBattleUnitBreakWeapon(u);
     return result;
 }
 
 void DisplayWRankUpPopup(u32 a) {
-    struct BattleUnit * something = 0;
+    struct BattleUnit *unit = 0;
     if (BattleUnit_ShouldDisplayWRankUp(&gBattleActor)) {
-        something = &gBattleActor;
+        unit = &gBattleActor;
     }
     if (BattleUnit_ShouldDisplayWRankUp(&gBattleTarget)) {
-        something = &gBattleTarget;
+        unit = &gBattleTarget;
     }
-    if (something) {
-        NewPopup_WRankIncrease(something->weaponType, a);
+    if (unit) {
+        NewPopup_WRankIncrease(unit->weaponType, a);
     }
 }
 
 s8 BattleUnit_ShouldDisplayWRankUp(struct BattleUnit *u) {
     u32 result;
-    if ((u->unit.index & 0xc0))
+    if (u->unit.index & 0xc0)
         return 0;
-    if (HasBattleUnitGainedWeaponLevel(u) == 0) {
+    if (HasBattleUnitGainedWeaponLevel(u) == 0)
         return 0;
-    }
     return 1;
 }
 
