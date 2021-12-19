254f434fcea620d33ba70410045595404bf1de44
Noah Nelson <nelson.noah96@gmail.com>
2021-12-19 05:31:32 +0000

    Split off data for branched promotion proc

---
+++ data/branched_promotion.s
@@ -0,0 +1,25 @@
+    .section .data
+
+	.global gUnknown_08B1271C
+gUnknown_08B1271C:  @ 0x08B1271C
+	.incbin "baserom.gba", 0xB1271C, 0xD0
+
+	.global gUnknown_08B127EC
+gUnknown_08B127EC:  @ 0x08B127EC
+	.incbin "baserom.gba", 0xB127EC, 0x20
+
+	.global gUnknown_08B1280C
+gUnknown_08B1280C:  @ 0x08B1280C
+	.incbin "baserom.gba", 0xB1280C, 0x124
+
+	.global gUnknown_08B12930
+gUnknown_08B12930:  @ 0x08B12930
+	.incbin "baserom.gba", 0xB12930, 0xB4
+
+	.global gUnknown_08B129E4
+gUnknown_08B129E4:  @ 0x08B129E4
+	.incbin "baserom.gba", 0xB129E4, 0x24
+
+	.global gUnknown_08B12A08
+gUnknown_08B12A08:  @ 0x08B12A08
+	.incbin "baserom.gba", 0xB12A08, 0x58
---
+++ data/data_B12A60.s
@@ -0,0 +1,129 @@
+    .section .data
+
+	.global gUnknown_08B12A60
+gUnknown_08B12A60:  @ 0x08B12A60
+	.incbin "baserom.gba", 0xB12A60, 0x4
+
+	.global gUnknown_08B12A64
+gUnknown_08B12A64:  @ 0x08B12A64
+	.incbin "baserom.gba", 0xB12A64, 0x40
+
+	.global gUnknown_08B12AA4
+gUnknown_08B12AA4:  @ 0x08B12AA4
+	.incbin "baserom.gba", 0xB12AA4, 0x4
+
+	.global gUnknown_08B12AA8
+gUnknown_08B12AA8:  @ 0x08B12AA8
+	.incbin "baserom.gba", 0xB12AA8, 0x1A
+
+	.global gUnknown_08B12AC2
+gUnknown_08B12AC2:  @ 0x08B12AC2
+	.incbin "baserom.gba", 0xB12AC2, 0xE
+
+	.global gUnknown_08B12AD0
+gUnknown_08B12AD0:  @ 0x08B12AD0
+	.incbin "baserom.gba", 0xB12AD0, 0x10
+
+	.global gUnknown_08B12AE0
+gUnknown_08B12AE0:  @ 0x08B12AE0
+	.incbin "baserom.gba", 0xB12AE0, 0x24
+
+	.global gUnknown_08B12B04
+gUnknown_08B12B04:  @ 0x08B12B04
+	.incbin "baserom.gba", 0xB12B04, 0x18
+
+	.global gUnknown_08B12B1C
+gUnknown_08B12B1C:  @ 0x08B12B1C
+	.incbin "baserom.gba", 0xB12B1C, 0x20
+
+	.global gUnknown_08B12B3C
+gUnknown_08B12B3C:  @ 0x08B12B3C
+	.incbin "baserom.gba", 0xB12B3C, 0x60
+
+	.global gUnknown_08B12B9C
+gUnknown_08B12B9C:  @ 0x08B12B9C
+	.incbin "baserom.gba", 0xB12B9C, 0x50
+
+	.global gUnknown_08B12BEC
+gUnknown_08B12BEC:  @ 0x08B12BEC
+	.incbin "baserom.gba", 0xB12BEC, 0x28
+
+	.global gUnknown_08B12C14
+gUnknown_08B12C14:  @ 0x08B12C14
+	.incbin "baserom.gba", 0xB12C14, 0x28
+
+	.global gUnknown_08B12C3C
+gUnknown_08B12C3C:  @ 0x08B12C3C
+	.incbin "baserom.gba", 0xB12C3C, 0x28
+
+	.global gUnknown_08B12C64
+gUnknown_08B12C64:  @ 0x08B12C64
+	.incbin "baserom.gba", 0xB12C64, 0xB8
+
+	.global gUnknown_08B12D1C
+gUnknown_08B12D1C:  @ 0x08B12D1C
+	.incbin "baserom.gba", 0xB12D1C, 0x98
+
+	.global gUnknown_08B12DB4
+gUnknown_08B12DB4:  @ 0x08B12DB4
+	.incbin "baserom.gba", 0xB12DB4, 0x4798
+
+	.global gUnknown_08B1754C
+gUnknown_08B1754C:  @ 0x08B1754C
+	.incbin "baserom.gba", 0xB1754C, 0x20
+
+	.global gUnknown_08B1756C
+gUnknown_08B1756C:  @ 0x08B1756C
+	.incbin "baserom.gba", 0xB1756C, 0x160
+
+	.global gUnknown_08B176CC
+gUnknown_08B176CC:  @ 0x08B176CC
+	.incbin "baserom.gba", 0xB176CC, 0xF4
+
+	.global gUnknown_08B177C0
+gUnknown_08B177C0:  @ 0x08B177C0
+	.incbin "baserom.gba", 0xB177C0, 0xA4
+
+	.global gUnknown_08B17864
+gUnknown_08B17864:  @ 0x08B17864
+	.incbin "baserom.gba", 0xB17864, 0x2E0
+
+	.global gUnknown_08B17B44
+gUnknown_08B17B44:  @ 0x08B17B44
+	.incbin "baserom.gba", 0xB17B44, 0x20
+
+	.global gUnknown_08B17B64
+gUnknown_08B17B64:  @ 0x08B17B64
+	.incbin "baserom.gba", 0xB17B64, 0x1204
+
+	.global gUnknown_08B18D68
+gUnknown_08B18D68:  @ 0x08B18D68
+	.incbin "baserom.gba", 0xB18D68, 0x16C
+
+	.global gUnknown_08B18ED4
+gUnknown_08B18ED4:  @ 0x08B18ED4
+	.incbin "baserom.gba", 0xB18ED4, 0x60
+
+	.global gUnknown_08B18F34
+gUnknown_08B18F34:  @ 0x08B18F34
+	.incbin "baserom.gba", 0xB18F34, 0x7A4
+
+	.global gUnknown_08B196D8
+gUnknown_08B196D8:  @ 0x08B196D8
+	.incbin "baserom.gba", 0xB196D8, 0x17C
+
+	.global gUnknown_08B19854
+gUnknown_08B19854:  @ 0x08B19854
+	.incbin "baserom.gba", 0xB19854, 0x20
+
+	.global gUnknown_08B19874
+gUnknown_08B19874:  @ 0x08B19874
+	.incbin "baserom.gba", 0xB19874, 0x578
+
+	.global gUnknown_08B19DEC
+gUnknown_08B19DEC:  @ 0x08B19DEC
+	.incbin "baserom.gba", 0xB19DEC, 0x20
+
+	.global gUnknown_08B19E0C
+gUnknown_08B19E0C:  @ 0x08B19E0C
+	.incbin "baserom.gba", 0xB19E0C, 0x5928
