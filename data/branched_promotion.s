86a91d7acf20881cf2da3c1800a1b769a3d6a6d1
Noah Nelson <nelson.noah96@gmail.com>
2022-01-02 05:50:45 +0000

    Move over the related rodata

--- data/branched_promotion.s
+++ data/branched_promotion.s
@@ -23,3 +23,29 @@ gUnknown_08B129E4:  @ 0x08B129E4
 	.global gUnknown_08B12A08
 gUnknown_08B12A08:  @ 0x08B12A08
 	.incbin "baserom.gba", 0xB12A08, 0x58
+
+    .section .rodata
+
+	.global gUnknown_08207044
+gUnknown_08207044:  @ 0x08207044
+	.incbin "baserom.gba", 0x207044, 0x38
+
+	.global gUnknown_0820707C
+gUnknown_0820707C:  @ 0x0820707C
+	.incbin "baserom.gba", 0x20707C, 0xC
+
+	.global gUnknown_08207088
+gUnknown_08207088:  @ 0x08207088
+	.incbin "baserom.gba", 0x207088, 0xC
+
+	.global gUnknown_08207094
+gUnknown_08207094:  @ 0x08207094
+	.incbin "baserom.gba", 0x207094, 0xC
+
+	.global gUnknown_082070A0
+gUnknown_082070A0:  @ 0x082070A0
+	.incbin "baserom.gba", 0x2070A0, 0xC
+
+	.global gUnknown_082070AC
+gUnknown_082070AC:  @ 0x082070AC
+	.incbin "baserom.gba", 0x2070AC, 0xE4
--- data/const_data_205B84.s
+++ data/const_data_205B84.s
@@ -229,27 +229,3 @@ gUnknown_08206FDC:  @ 0x08206FDC
 	.global gUnknown_08206FE4
 gUnknown_08206FE4:  @ 0x08206FE4
 	.incbin "baserom.gba", 0x206FE4, 0x60
-
-	.global gUnknown_08207044
-gUnknown_08207044:  @ 0x08207044
-	.incbin "baserom.gba", 0x207044, 0x38
-
-	.global gUnknown_0820707C
-gUnknown_0820707C:  @ 0x0820707C
-	.incbin "baserom.gba", 0x20707C, 0xC
-
-	.global gUnknown_08207088
-gUnknown_08207088:  @ 0x08207088
-	.incbin "baserom.gba", 0x207088, 0xC
-
-	.global gUnknown_08207094
-gUnknown_08207094:  @ 0x08207094
-	.incbin "baserom.gba", 0x207094, 0xC
-
-	.global gUnknown_082070A0
-gUnknown_082070A0:  @ 0x082070A0
-	.incbin "baserom.gba", 0x2070A0, 0xC
-
-	.global gUnknown_082070AC
-gUnknown_082070AC:  @ 0x082070AC
-	.incbin "baserom.gba", 0x2070AC, 0xE4
