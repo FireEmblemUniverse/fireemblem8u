be0cd96ee6a1fcbf69b521f8e5f182d59ca75a7b
Eebit <eight_bit_hero@yahoo.ca>
2022-02-03 13:18:20 -0500

    (bmcontainer) Decompile sub_80315CC, GetConvoyItemSlot, HasConvoyAccess
    It looks like sub_80315CC is most likely "RemoveItemFromConvoy". This could use some cleanup as well.

--- src/bmcontainer.c
+++ src/bmcontainer.c
@@ -1,9 +1,12 @@
 #include "global.h"
 
 #include "variables.h"
+#include "bmitem.h"
 
 // 0203A8E4 - 0203A81C = C8 (200 in decimal)
 EWRAM_DATA u16 gUnknown_0203A81C[100] = {0};
+
+extern unsigned int sub_80BD048();
 
 u16* GetConvoyItemArray() {
     return gUnknown_0203A81C;
@@ -66,4 +69,53 @@ int AddItemToConvoy(int itemNameId) {
     }
     gUnknown_0202BCB0.itemUnk2E = itemNameId;
     return -1;
+}
+
+void sub_80315CC(int index) {
+    gUnknown_0203A81C[index] = 0;
+    ShrinkConvoyItemList();
+    return;
+}
+
+int GetConvoyItemSlot(int r0) {
+    int r2;
+    u16* convoy;
+    r0 = GetItemIndex(r0);
+    r2 = 0;
+    convoy = gUnknown_0203A81C;
+     for (r2 = 0; r2 <= 0x63; ++r2) {
+        // _080315F4
+        if (r0 == (convoy[r2] & 0xFF)) {
+            return r2;
+        }
+        // _08031608
+    }
+
+    return -1;
+}
+
+bool8 HasConvoyAccess() {
+    unsigned int chapterId;
+    s16 r1;
+    if (GMAP_STATE_BIT0 & gUnknown_03005280.state) { // if GMAP_STATE_BIT0 is set
+        chapterId = sub_80BD048();
+        if (chapterId <= 1) { //bls _08031650 
+            return 0;
+        } else if (chapterId == 0x38) { // beq _08031650
+            return 0;
+        } else {
+            // _08031658
+            return 1;
+        }
+    }
+    // _0803163C
+    r1 = gRAMChapterData.chapterIndex; // ch data @ 0xe
+    if (r1 <= 1) { // ble _08031650
+        return 0;
+    } else if (r1 == 0x38) { // beq _08031650
+        return 0;
+    } else if (r1 != 5) {
+        return 1;
+    }
+    return 0;
 }
