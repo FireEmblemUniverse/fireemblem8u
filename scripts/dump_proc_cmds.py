03c3d48e09e0500138033f8529ad810a3dbe1d30
Noah Nelson <nelson.noah96@gmail.com>
2022-08-02 16:54:59 +0000

    Refactor dump_proc_cmds
    To make it easier to resolve pointers and print in a format that's
    copy-pastable to assembly

--- scripts/dump_proc_cmds.py
+++ scripts/dump_proc_cmds.py
@@ -3,25 +3,23 @@
 import struct
 import sys
 
-INVALID = "Invalid proc cmd"
-
 
 def make_simple_proc(name, dataImm, dataPtr):
     if dataImm != 0 or dataPtr != 0:
-        return INVALID
-    return name
+        return None
+    return (name, None, None)
 
 
 def make_pointer_proc(name, dataImm, expImm, dataPtr):
     if dataImm != expImm:
-        return INVALID
-    return f"{name}({hex(dataPtr)})"
+        return None
+    return (name, None, dataPtr)
 
 
 def make_label_proc(name, dataImm, dataPtr):
     if dataPtr != 0:
-        return INVALID
-    return f"{name}({hex(dataImm)})"
+        return None
+    return (name, dataImm, None)
 
 
 def decode_proc_cmd(cmd):
@@ -56,7 +54,7 @@ def decode_proc_cmd(cmd):
         return make_pointer_proc("PROC_JUMP", dataImm, 0, dataPtr)
     if opcode == 0xE:
         if dataImm == 0 and dataPtr == 0:
-            return "PROC_YIELD"
+            return ("PROC_YIELD", None, None)
         return make_label_proc("PROC_SLEEP", dataImm, dataPtr)
     if opcode == 0xF:
         return make_label_proc("PROC_MARK", dataImm, dataPtr)
@@ -77,126 +75,190 @@ def decode_proc_cmd(cmd):
     if opcode == 0x17:
         return make_simple_proc("PROC_END_DUPLICATES", dataImm, dataPtr)
     if opcode == 0x18:
-        return f"PROC_CALL_ARG({hex(dataPtr)}, {hex(dataImm)})"
+        return ("PROC_CALL_ARG", dataPtr, dataImm)
     if opcode == 0x19:
         return make_simple_proc("PROC_19", dataImm, dataPtr)
-    return INVALID
+    return None
 
 
 def assert_eq(a, b):
     assert a == b, f"{a} != {b}"
 
 
-def test():
-    assert_eq(decode_proc_cmd(b"\x00\x00\x00\x00\x00\x00\x00\x00"), "PROC_END")
-    assert_eq(decode_proc_cmd(b"\x00\x00\x01\x00\x00\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x00\x00\x00\x00\x00\x00\x01\x00"), INVALID)
+def test_decode():
     assert_eq(
-        decode_proc_cmd(b"\x01\x00\x00\x00\x00\x00\x00\x01"), "PROC_NAME(0x1000000)"
+        decode_proc_cmd(b"\x00\x00\x00\x00\x00\x00\x00\x00"), ("PROC_END", None, None)
     )
-    assert_eq(decode_proc_cmd(b"\x01\x00\x01\x00\x00\x00\x00\x00"), INVALID)
+    assert_eq(decode_proc_cmd(b"\x00\x00\x01\x00\x00\x00\x00\x00"), None)
+    assert_eq(decode_proc_cmd(b"\x00\x00\x00\x00\x00\x00\x01\x00"), None)
     assert_eq(
-        decode_proc_cmd(b"\x02\x00\x00\x00\x12\x34\x56\x78"), "PROC_CALL(0x78563412)"
+        decode_proc_cmd(b"\x01\x00\x00\x00\x00\x00\x00\x01"),
+        ("PROC_NAME", None, 0x1000000),
     )
-    assert_eq(decode_proc_cmd(b"\x02\x00\x01\x00\x12\x34\x56\x78"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x03\x00\x00\x00\x12\x00\x00\x00"), "PROC_REPEAT(0x12)")
+    assert_eq(decode_proc_cmd(b"\x01\x00\x01\x00\x00\x00\x00\x00"), None)
     assert_eq(
-        decode_proc_cmd(b"\x04\x00\x00\x00\x12\x00\x00\x00"), "PROC_SET_END_CB(0x12)"
+        decode_proc_cmd(b"\x02\x00\x00\x00\x12\x34\x56\x78"),
+        ("PROC_CALL", None, 0x78563412),
+    )
+    assert_eq(decode_proc_cmd(b"\x02\x00\x01\x00\x12\x34\x56\x78"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x03\x00\x00\x00\x12\x00\x00\x00"),
+        ("PROC_REPEAT", None, 0x12),
     )
     assert_eq(
-        decode_proc_cmd(b"\x05\x00\x00\x00\x12\x00\x00\x00"), "PROC_START_CHILD(0x12)"
+        decode_proc_cmd(b"\x04\x00\x00\x00\x12\x00\x00\x00"),
+        ("PROC_SET_END_CB", None, 0x12),
+    )
+    assert_eq(
+        decode_proc_cmd(b"\x05\x00\x00\x00\x12\x00\x00\x00"),
+        ("PROC_START_CHILD", None, 0x12),
     )
     assert_eq(
         decode_proc_cmd(b"\x06\x00\x01\x00\x12\x00\x00\x00"),
-        "PROC_START_CHILD_BLOCKING(0x12)",
+        ("PROC_START_CHILD_BLOCKING", None, 0x12),
     )
-    assert_eq(decode_proc_cmd(b"\x06\x00\x00\x00\x12\x00\x00\x00"), INVALID)
+    assert_eq(decode_proc_cmd(b"\x06\x00\x00\x00\x12\x00\x00\x00"), None)
     assert_eq(
         decode_proc_cmd(b"\x07\x00\x00\x00\x12\x00\x00\x00"),
-        "PROC_START_MAIN_BUGGED(0x12)",
+        ("PROC_START_MAIN_BUGGED", None, 0x12),
     )
     assert_eq(
         decode_proc_cmd(b"\x08\x00\x00\x00\x12\x00\x00\x00"),
-        "PROC_WHILE_EXISTS(0x12)",
+        ("PROC_WHILE_EXISTS", None, 0x12),
     )
     assert_eq(
         decode_proc_cmd(b"\x09\x00\x00\x00\x12\x00\x00\x00"),
-        "PROC_END_EACH(0x12)",
+        ("PROC_END_EACH", None, 0x12),
     )
     assert_eq(
         decode_proc_cmd(b"\x0a\x00\x00\x00\x12\x00\x00\x00"),
-        "PROC_BREAK_EACH(0x12)",
+        ("PROC_BREAK_EACH", None, 0x12),
     )
-    assert_eq(decode_proc_cmd(b"\x0b\x00\x00\x00\x12\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x0b\x00\x10\x00\x00\x00\x00\x00"), "PROC_LABEL(0x10)")
-    assert_eq(decode_proc_cmd(b"\x0c\x00\x10\x00\x00\x00\x00\x00"), "PROC_GOTO(0x10)")
-    assert_eq(decode_proc_cmd(b"\x0d\x00\x10\x00\x00\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x0d\x00\x00\x00\x00\x10\x00\x00"), "PROC_JUMP(0x1000)")
-    assert_eq(decode_proc_cmd(b"\x0e\x00\x00\x00\x12\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x0e\x00\x10\x00\x00\x00\x00\x00"), "PROC_SLEEP(0x10)")
-    assert_eq(decode_proc_cmd(b"\x0f\x00\x00\x00\x12\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x0f\x00\x10\x00\x00\x00\x00\x00"), "PROC_MARK(0x10)")
-    assert_eq(decode_proc_cmd(b"\x10\x00\x00\x00\x00\x00\x00\x00"), "PROC_BLOCK")
-    assert_eq(decode_proc_cmd(b"\x10\x00\x01\x00\x00\x00\x00\x00"), INVALID)
+    assert_eq(decode_proc_cmd(b"\x0b\x00\x00\x00\x12\x00\x00\x00"), None)
     assert_eq(
-        decode_proc_cmd(b"\x11\x00\x00\x00\x00\x00\x00\x00"), "PROC_END_IF_DUPLICATE"
+        decode_proc_cmd(b"\x0b\x00\x10\x00\x00\x00\x00\x00"), ("PROC_LABEL", 0x10, None)
     )
-    assert_eq(decode_proc_cmd(b"\x11\x00\x01\x00\x00\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x12\x00\x00\x00\x00\x00\x00\x00"), "PROC_SET_BIT4")
-    assert_eq(decode_proc_cmd(b"\x12\x00\x01\x00\x00\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x13\x00\x00\x00\x00\x00\x00\x00"), "PROC_13")
-    assert_eq(decode_proc_cmd(b"\x13\x00\x01\x00\x00\x00\x00\x00"), INVALID)
+    assert_eq(
+        decode_proc_cmd(b"\x0c\x00\x10\x00\x00\x00\x00\x00"), ("PROC_GOTO", 0x10, None)
+    )
+    assert_eq(decode_proc_cmd(b"\x0d\x00\x10\x00\x00\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x0d\x00\x00\x00\x00\x10\x00\x00"),
+        ("PROC_JUMP", None, 0x1000),
+    )
+    assert_eq(decode_proc_cmd(b"\x0e\x00\x00\x00\x12\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x0e\x00\x10\x00\x00\x00\x00\x00"), ("PROC_SLEEP", 0x10, None)
+    )
+    assert_eq(decode_proc_cmd(b"\x0f\x00\x00\x00\x12\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x0f\x00\x10\x00\x00\x00\x00\x00"), ("PROC_MARK", 0x10, None)
+    )
+    assert_eq(
+        decode_proc_cmd(b"\x10\x00\x00\x00\x00\x00\x00\x00"), ("PROC_BLOCK", None, None)
+    )
+    assert_eq(decode_proc_cmd(b"\x10\x00\x01\x00\x00\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x11\x00\x00\x00\x00\x00\x00\x00"),
+        ("PROC_END_IF_DUPLICATE", None, None),
+    )
+    assert_eq(decode_proc_cmd(b"\x11\x00\x01\x00\x00\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x12\x00\x00\x00\x00\x00\x00\x00"),
+        ("PROC_SET_BIT4", None, None),
+    )
+    assert_eq(decode_proc_cmd(b"\x12\x00\x01\x00\x00\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x13\x00\x00\x00\x00\x00\x00\x00"), ("PROC_13", None, None)
+    )
+    assert_eq(decode_proc_cmd(b"\x13\x00\x01\x00\x00\x00\x00\x00"), None)
     assert_eq(
         decode_proc_cmd(b"\x14\x00\x00\x00\x12\x00\x00\x00"),
-        "PROC_WHILE(0x12)",
+        ("PROC_WHILE", None, 0x12),
     )
-    assert_eq(decode_proc_cmd(b"\x15\x00\x00\x00\x00\x00\x00\x00"), "PROC_15")
-    assert_eq(decode_proc_cmd(b"\x15\x00\x01\x00\x00\x00\x00\x00"), INVALID)
+    assert_eq(
+        decode_proc_cmd(b"\x15\x00\x00\x00\x00\x00\x00\x00"), ("PROC_15", None, None)
+    )
+    assert_eq(decode_proc_cmd(b"\x15\x00\x01\x00\x00\x00\x00\x00"), None)
     assert_eq(
         decode_proc_cmd(b"\x16\x00\x00\x00\x12\x00\x00\x00"),
-        "PROC_CALL_2(0x12)",
+        ("PROC_CALL_2", None, 0x12),
     )
     assert_eq(
-        decode_proc_cmd(b"\x17\x00\x00\x00\x00\x00\x00\x00"), "PROC_END_DUPLICATES"
+        decode_proc_cmd(b"\x17\x00\x00\x00\x00\x00\x00\x00"),
+        ("PROC_END_DUPLICATES", None, None),
     )
-    assert_eq(decode_proc_cmd(b"\x17\x00\x01\x00\x00\x00\x00\x00"), INVALID)
+    assert_eq(decode_proc_cmd(b"\x17\x00\x01\x00\x00\x00\x00\x00"), None)
     assert_eq(
         decode_proc_cmd(b"\x18\x00\x00\x04\x12\x34\x00\x00"),
-        "PROC_CALL_ARG(0x3412, 0x400)",
+        ("PROC_CALL_ARG", 0x3412, 0x400),
     )
-    assert_eq(decode_proc_cmd(b"\x19\x00\x00\x00\x00\x00\x00\x00"), "PROC_19")
-    assert_eq(decode_proc_cmd(b"\x19\x00\x01\x00\x00\x00\x00\x00"), INVALID)
-    assert_eq(decode_proc_cmd(b"\x0e\x00\x00\x00\x00\x00\x00\x00"), "PROC_YIELD")
+    assert_eq(
+        decode_proc_cmd(b"\x19\x00\x00\x00\x00\x00\x00\x00"), ("PROC_19", None, None)
+    )
+    assert_eq(decode_proc_cmd(b"\x19\x00\x01\x00\x00\x00\x00\x00"), None)
+    assert_eq(
+        decode_proc_cmd(b"\x0e\x00\x00\x00\x00\x00\x00\x00"), ("PROC_YIELD", None, None)
+    )
 
 
 def read_procs(f, start_off, end_off):
+    procs_found = []
     length = end_off - start_off
     current_off = start_off
     current_script = []
     current_script_start = 0
     f.seek(start_off)
-    while current_off < end_off:
+    while current_off <= end_off - 8:
         cmd = f.read(8)
         proc = decode_proc_cmd(cmd)
-        if proc == INVALID:
+        if proc is None:
             current_script_start = 0
             current_script = []
-        elif proc != "PROC_END":
+        elif proc[0] != "PROC_END":
             if current_script_start == 0:
                 current_script_start = current_off
             current_script.append(proc)
         else:
-            assert proc == "PROC_END"
+            assert proc[0] == "PROC_END"
             if current_script:
                 current_script.append(proc)
-                print(f"Found script at {hex(current_script_start)}")
-                print(f"Length {hex(len(current_script) * 8)}")
-                for cmd in current_script:
-                    print(cmd)
+                procs_found.append((current_script_start, current_script[:]))
                 current_script_start = 0
                 current_script = []
 
         current_off += 8
+    return procs_found
+
+
+def smoketest():
+    with open("baserom.gba", "rb") as f:
+        procs = read_procs(f, 0xB12C14, 0xB12C6C)
+        assert_eq(
+            procs,
+            [
+                (
+                    0xB12C14,
+                    [
+                        ("PROC_NAME", None, 136343888),
+                        ("PROC_SLEEP", 1, None),
+                        ("PROC_CALL", None, 135062409),
+                        ("PROC_CALL", None, 135062245),
+                        ("PROC_END", None, None),
+                    ],
+                ),
+                (
+                    0xB12C3C,
+                    [
+                        ("PROC_NAME", None, 136343912),
+                        ("PROC_SLEEP", 1, None),
+                        ("PROC_CALL", None, 135063901),
+                        ("PROC_REPEAT", None, 135064041),
+                        ("PROC_END", None, None),
+                    ],
+                ),
+            ],
+        )
 
 
 if __name__ == "__main__":
@@ -204,10 +266,16 @@ if __name__ == "__main__":
         print("Usage: either test or specify a data symbol to read as proc script")
         sys.exit(1)
     if sys.argv[1] == "test":
-        test()
+        test_decode()
+        smoketest()
         print("Ok")
     else:
         with open("baserom.gba", "rb") as f:
             start_off = int(sys.argv[1], base=16)
             end_off = int(sys.argv[2], base=16)
-            read_procs(f, start_off, end_off)
+            procs = read_procs(f, start_off, end_off)
+            for start, proc in procs:
+                print(f"Found script at {hex(start)}")
+                print(f"Length {hex(len(proc) * 8)}")
+                for cmd in proc:
+                    print(cmd)
