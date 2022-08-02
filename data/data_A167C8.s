67136d83220d2557fb88ed70761e862f4cb12f6f
Noah Nelson <nelson.noah96@gmail.com>
2022-08-02 07:18:57 +0000

    proof of concept - dump a proc

--- data/data_A167C8.s
+++ data/data_A167C8.s
@@ -3186,7 +3186,63 @@ gUnknown_08B12C64:  @ 0x08B12C64
 
 	.global gUnknown_08B12D1C
 gUnknown_08B12D1C:  @ 0x08B12D1C
-	.incbin "baserom.gba", 0xB12D1C, 0x98
+    @ PROC_NAME(E_Guide)
+    .short 0x1, 0x0
+    .word gUnknown_082070AC + 0xd0
+    @ PROC_CALL(AddSkipThread2)
+    .short 0x2, 0x0
+    .word AddSkipThread2
+    @ PROC_CALL(BMapDispSuspend)
+    .short 0x2, 0x0
+    .word BMapDispSuspend
+    @ PROC_YIELD
+    .short 0xe, 0x0
+    .word 0x0
+    @ PROC_CALL(sub_80CECB0)
+    .short 0x2, 0x0
+    .word sub_80CECB0
+    @ PROC_CALL(NewGreenTextColorManager)
+    .short 0x2, 0x0
+    .word NewGreenTextColorManager
+    @ PROC_CALL(sub_8013DA4)
+    .short 0x2, 0x0
+    .word sub_8013DA4
+    @ PROC_REPEAT(ContinueUntilSomeTransistion6CExists)
+    .short 0x3, 0x0
+    .word ContinueUntilSomeTransistion6CExists
+    @ PROC_CALL(sub_80CEF10)
+    .short 0x2, 0x0
+    .word sub_80CEF10
+    @ PROC_REPEAT(sub_80CEFD4)
+    .short 0x3, 0x0
+    .word sub_80CEFD4
+    @ PROC_CALL(sub_8013D80)
+    .short 0x2, 0x0
+    .word sub_8013D80
+    @ PROC_REPEAT(ContinueUntilSomeTransistion6CExists)
+    .short 0x3, 0x0
+    .word ContinueUntilSomeTransistion6CExists
+    @ PROC_CALL(EndGreenTextColorManager)
+    .short 0x2, 0x0
+    .word EndGreenTextColorManager
+    @ PROC_CALL(sub_80CF448)
+    .short 0x2, 0x0
+    .word sub_80CF448
+    @ PROC_YIELD
+    .short 0xe, 0x0
+    .word 0x0
+    @ PROC_CALL(BMapDispResume)
+    .short 0x2, 0x0
+    .word BMapDispResume
+    @ PROC_CALL(RefreshBMapGraphics)
+    .short 0x2, 0x0
+    .word RefreshBMapGraphics
+    @ PROC_CALL(SubSkipThread2)
+    .short 0x2, 0x0
+    .word SubSkipThread2
+    @ PROC_END
+    .short 0x0, 0x0
+    .word 0x0
 
 	.global gUnknown_08B12DB4
 gUnknown_08B12DB4:  @ 0x08B12DB4
