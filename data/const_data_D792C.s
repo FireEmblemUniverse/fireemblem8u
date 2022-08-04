06a1b5eb4311551a4927e0bee4d4e66552b02eac
Noah Nelson <nelson.noah96@gmail.com>
2022-08-04 05:26:31 +0000

    Find and dump another pointer table
    With this one, can actually get to the map part of the prologue

--- data/const_data_D792C.s
+++ data/const_data_D792C.s
@@ -26,7 +26,31 @@ gUnknown_080D7954:  @ 0x080D7954
 
     .global gUnknown_080D7964
 gUnknown_080D7964:  @ 0x080D7964
-    .incbin "baserom.gba", 0xD7964, 0x60
+@ replacing .incbin "baserom.gba", 0x000d7964, 0x60
+    .4byte Proc_Start
+    .4byte sub_80018E4
+    .4byte 0x00000001
+    .4byte Proc_Start
+    .4byte sub_8001860
+    .4byte 0xffffffff
+    .4byte Proc_StartBlocking
+    .4byte sub_80018E4
+    .4byte 0x00000001
+    .4byte Proc_StartBlocking
+    .4byte sub_8001860
+    .4byte 0xffffffff
+    .4byte Proc_Start
+    .4byte sub_80019E8
+    .4byte 0xffffffff
+    .4byte Proc_Start
+    .4byte sub_8001964
+    .4byte 0x00000001
+    .4byte Proc_StartBlocking
+    .4byte sub_80019E8
+    .4byte 0xffffffff
+    .4byte Proc_StartBlocking
+    .4byte sub_8001964
+    .4byte 0x00000001
 
     .global gUnknown_080D79C4
 gUnknown_080D79C4:  @ 0x080D79C4
@@ -35,4 +59,5 @@ gUnknown_080D79C4:  @ 0x080D79C4
     .global gUnknown_080D79D0
 gUnknown_080D79D0:  @ 0x080D79D0
     .incbin "baserom.gba", 0xD79D0, 0x8
+
 
