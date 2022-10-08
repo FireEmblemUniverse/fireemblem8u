43370e2d40cc836ea18b5c1340eefcb478998436
MokhaLeee <nkulyc@163.com>
2022-10-08 21:17:38 +0800

    (emitstarfx) code review

--- include/bmfx.h
+++ include/bmfx.h
@@ -57,7 +57,7 @@ void StartDanceringAnim(ProcPtr parent);
 void StartEventWarpAnim(ProcPtr parent, int xCamera, int yCamera, s8 xWarp, s8 yWarp);
 void StartEventWarpAnim_unused(ProcPtr parent, int xCamera, int yCamera, s8 xWarp);
 int EventWarpAnimExists();
-void StartEmitStarsAnim(ProcPtr parent, int r1, int r2, int r3, int r4);
+void StartEmitStarsAnim(ProcPtr parent, int xloc, int yloc, int xdiff, int ydiff);
 void ClearEmitedStars();
 void EndEmitStars();
 #endif /* GUARD_BMFX_H */
