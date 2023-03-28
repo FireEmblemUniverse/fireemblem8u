4a74fd452b5e551514d76c5bd02d1d5148f53982
mokhaleee <nkulyc@163.com>
2023-03-28 11:57:53 +0800

    (ekrlevelup) decompiling
    Since the rest asm code involves AIS struct and AIS core struct which is undefined for now, so I think that a better idea is to decomp code-banim first, and then come back to this ekrlevelup

---
+++ data/const_data_E162A.s
@@ -0,0 +1,101 @@
+	.section .rodata
+
+	.global gUnknown_080E1664
+gUnknown_080E1664:  @ 0x080E1664
+	.incbin "baserom.gba", 0xE1664, 0x36
+
+	.global gUnknown_080E169A
+gUnknown_080E169A:  @ 0x080E169A
+	.incbin "baserom.gba", 0xE169A, 0x42
+
+	.global gUnknown_080E16DC
+gUnknown_080E16DC:  @ 0x080E16DC
+	.incbin "baserom.gba", 0xE16DC, 0x56
+
+	.global gUnknown_080E1732
+gUnknown_080E1732:  @ 0x080E1732
+	.incbin "baserom.gba", 0xE1732, 0xE
+
+	.global gUnknown_080E1740
+gUnknown_080E1740:  @ 0x080E1740
+	.incbin "baserom.gba", 0xE1740, 0x80
+
+	.global gUnknown_080E17C0
+gUnknown_080E17C0:  @ 0x080E17C0
+	.incbin "baserom.gba", 0xE17C0, 0x9A4
+
+	.global gUnknown_080E2164
+gUnknown_080E2164:  @ 0x080E2164
+	.incbin "baserom.gba", 0xE2164, 0x9F0
+
+	.global gUnknown_080E2B54
+gUnknown_080E2B54:  @ 0x080E2B54
+	.incbin "baserom.gba", 0xE2B54, 0x1024
+
+	.global gUnknown_080E3B78
+gUnknown_080E3B78:  @ 0x080E3B78
+	.incbin "baserom.gba", 0xE3B78, 0x9B8
+
+	.global gUnknown_080E4530
+gUnknown_080E4530:  @ 0x080E4530
+	.incbin "baserom.gba", 0xE4530, 0x9CC
+
+	.global gUnknown_080E4EFC
+gUnknown_080E4EFC:  @ 0x080E4EFC
+	.incbin "baserom.gba", 0xE4EFC, 0x9A8
+
+	.global gUnknown_080E58A4
+gUnknown_080E58A4:  @ 0x080E58A4
+	.incbin "baserom.gba", 0xE58A4, 0x320
+
+	.global gUnknown_080E5BC4
+gUnknown_080E5BC4:  @ 0x080E5BC4
+	.incbin "baserom.gba", 0xE5BC4, 0x3D4
+
+	.global gUnknown_080E5F98
+gUnknown_080E5F98:  @ 0x080E5F98
+	.incbin "baserom.gba", 0xE5F98, 0x434
+
+	.global gUnknown_080E63CC
+gUnknown_080E63CC:  @ 0x080E63CC
+	.incbin "baserom.gba", 0xE63CC, 0x3C0
+
+	.global gUnknown_080E678C
+gUnknown_080E678C:  @ 0x080E678C
+	.incbin "baserom.gba", 0xE678C, 0x538
+
+	.global gUnknown_080E6CC4
+gUnknown_080E6CC4:  @ 0x080E6CC4
+	.incbin "baserom.gba", 0xE6CC4, 0x73C
+
+	.global gUnknown_080E7400
+gUnknown_080E7400:  @ 0x080E7400
+	.incbin "baserom.gba", 0xE7400, 0x78C
+
+	.global gUnknown_080E7B8C
+gUnknown_080E7B8C:  @ 0x080E7B8C
+	.incbin "baserom.gba", 0xE7B8C, 0x78C
+
+	.global gUnknown_080E8318
+gUnknown_080E8318:  @ 0x080E8318
+	.incbin "baserom.gba", 0xE8318, 0x6
+
+	.global gUnknown_080E831E
+gUnknown_080E831E:  @ 0x080E831E
+	.incbin "baserom.gba", 0xE831E, 0x16
+
+	.global gUnknown_080E8334
+gUnknown_080E8334:  @ 0x080E8334
+	.incbin "baserom.gba", 0xE8334, 0x1A
+
+	.global gUnknown_080E834E
+gUnknown_080E834E:  @ 0x080E834E
+	.incbin "baserom.gba", 0xE834E, 0x1E
+
+	.global gUnknown_080E836C
+gUnknown_080E836C:  @ 0x080E836C
+	.incbin "baserom.gba", 0xE836C, 0x22
+
+	.global gUnknown_080E838E
+gUnknown_080E838E:  @ 0x080E838E
+	.incbin "baserom.gba", 0xE838E, 0x86
