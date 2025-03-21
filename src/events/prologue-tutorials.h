e681effbbd47a611f5c9ad37171cdacdf0481090
minirop <minirop@hotmail.com>
2025-03-21 22:31:12 +0100

    typo "Emimy" -> "Enemy"

--- src/events/prologue-tutorials.h
+++ src/events/prologue-tutorials.h
@@ -231,8 +231,8 @@ CONST_DATA EventListScr EventScr_Prologue_Tutorial9[] = {
     KILL(-2)
     DISA_IF(-2)
 
-    ENUT(EVFLAG_TMP(7)) /* trigger event: EventScr_Prologue_OneEmimyLeft */
-    CALL(EventScr_Prologue_OneEmimyLeft)
+    ENUT(EVFLAG_TMP(7)) /* trigger event: EventScr_Prologue_OneEnemyLeft */
+    CALL(EventScr_Prologue_OneEnemyLeft)
     SET_ENDTURN(CHARACTER_SETH)
 
     NoFade
